6b13c458c0acda19ba5f647b13fb17ba219ff64d
JAKE NUSSBAUM <jacob.nussbaum@cooper.edu>
2021-10-15 21:42:27 +0000

    My completed homework

--- HW2/prob-2/mycode.c
+++ HW2/prob-2/mycode.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
-int main(void) {
-	printf("Hello, World!\n");
+int main() {
+
+	int inputminutes, hours, minutes;
+	printf("Input minutes: ");
+	scanf("%d", &inputminutes);
+	hours = (inputminutes/60); 
+	minutes = (inputminutes -(60*hours));
+	printf("Hours: %d", hours);
+    printf("Minutes: %d", minutes);
+	
+	return 0;
+
 }
--- HW2/prob-3/mycode.c
+++ HW2/prob-3/mycode.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
-int main(void) {
-	printf("Hello, World!\n");
+float floatinput;    
+int intinput;         
+char characterinput;  
+
+int main() {
+	floatinput = 5.6;
+	intinput = 7;
+	characterinput = 'J';
+
+	printf("%f as %%d: %d\n", floatinput, floatinput);
+	printf("%d as %%f: %f\n", intinput, intinput);
+	printf("%c as %%d: %d\n", characterinput, characterinput);
+
+	return(0);
 }
