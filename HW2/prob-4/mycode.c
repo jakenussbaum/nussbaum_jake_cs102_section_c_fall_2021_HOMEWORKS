bbc538a241ee0c1cdaddbdf7f9576e87c0edf347
jakenussbaum <91992298+jakenussbaum@users.noreply.github.com>
2021-10-18 00:20:49 -0400

    Update mycode.c

--- HW2/prob-4/mycode.c
+++ HW2/prob-4/mycode.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
-
+ 
 int main(void) {
-	printf("Hello, World!\n");
-}
+
+	int inputcents, quarters, dimes, nickels, pennies;
+	printf("Input cents: ");
+	scanf("%d", &inputcents);
+	quarters = (inputcents/25);
+	dimes = ((inputcents -(25*quarters))/10);
+	nickels = ((inputcents -(25*quarters)-(10*dimes))/5);
+	pennies = (inputcents -(25*quarters)-(10*dimes)-(5*nickels));
+	printf("Quarters: %d", quarters);
+	printf(" Dimes: %d", dimes);
+	printf(" Nickels: %d", nickels);
+	printf(" Pennies: %d ", pennies);
+ 
+	return 0;
+ 
+}          
