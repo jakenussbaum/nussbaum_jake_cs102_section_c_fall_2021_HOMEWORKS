3d54c92d86e8b95af25edf7e4b60a2de4b3770c1
jakenussbaum <91992298+jakenussbaum@users.noreply.github.com>
2021-10-18 00:05:50 -0400

    Update mycode.c

--- HW2/prob-1/mycode.c
+++ HW2/prob-1/mycode.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
-int main(void) {
-	printf("Hello, World!\n");
+int main()
+{
+  float height, width, perimeter;
+  printf("Enter height: ");
+  scanf("%f", &height);
+  printf("Enter width: ");
+  scanf("%f", &width);
+  perimeter = (2*height)+(2*width);
+  printf("Perimeter of rectangle = %f ", perimeter);
+
+  return 0;
 }
